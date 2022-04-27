35acb52f4aa4a904a5cf6dd851e68fd57821c57e
CEI <admin@41.local>
2022-04-27 18:52:25 +0200

    Ejercicio 16

---
+++ Ejercicio16/ejercicio16.js
@@ -0,0 +1,5 @@
+var numero = document.getElementById('numero')
+var resultado = document.getElementById('resultado')
+var boton = document.getElementById('btn')
+
+console.log(typeof(numero.value));
