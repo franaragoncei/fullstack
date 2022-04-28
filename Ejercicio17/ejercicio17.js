2d7dbdff044eec499ca39a71d764281c65e1c42d
CEI <admin@41.local>
2022-04-28 16:41:07 +0200

    Actualiza ehejercicio 17 con número aleatorio

--- Ejercicio17/ejercicio17.js
+++ Ejercicio17/ejercicio17.js
@@ -1,14 +1,16 @@
 var numero = document.getElementById('numero')
 var boton = document.getElementById('btn')
 var respuesta = document.getElementById('resultado')
-var numeroEscogido = 20
+var numeroEscogido = Math.floor(Math.random() * 100) + 1
 
 boton.addEventListener('click', function(){
+    console.log(numeroEscogido);
     if(numero.value > numeroEscogido){
         respuesta.innerHTML = 'Te pasaste'
     }else if(numero.value < numeroEscogido){
         respuesta.innerHTML = 'No has llegado'
     }else{
         respuesta.innerHTML = 'Acertaste'
+        numeroEscogido = Math.floor(Math.random() * 100) + 1
     }
 })
