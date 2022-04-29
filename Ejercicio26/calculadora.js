feda4e4435a5b543c2c1b4c3fe5e7b69a47ad8fd
CEI <admin@41.local>
2022-04-29 18:00:35 +0200

    Añade javascript a la calculadora

---
+++ Ejercicio26/calculadora.js
@@ -0,0 +1,51 @@
+var numeros = document.querySelectorAll('.number')
+var operadores = document.querySelectorAll('.operator')
+var igual = document.getElementById('igual')
+var resultado = document.getElementById('resultado')
+var numero1
+var numero2
+var operador
+var resultadoOperacion = 0
+
+numeros.forEach(elemento => {
+    elemento.addEventListener('click', function () {
+        if (numero1 == undefined) {
+            numero1 = elemento.value
+        } else {
+            numero2 = elemento.value
+        }
+        resultado.innerHTML = elemento.value
+    })
+})
+
+operadores.forEach(elemento => {
+    elemento.addEventListener('click', function () {
+        operador = elemento.value
+    })
+})
+
+igual.addEventListener('click', function () {
+    if (numero1 == undefined) {
+        resultado.innerHTML = resultadoOperacion
+    } else {
+        switch (operador) {
+            case '+':
+                resultadoOperacion = parseInt(numero1) + parseInt(numero2)
+                break;
+            case '-':
+                resultadoOperacion = parseInt(numero1) - parseInt(numero2)
+                break;
+            case 'X':
+                resultadoOperacion = parseInt(numero1) * parseInt(numero2)
+                break;
+            case '/':
+                resultadoOperacion = parseInt(numero1) / parseInt(numero2)
+                break;
+            default:
+                resultadoOperacion = 0
+        }
+        numero1 = undefined
+        resultado.innerHTML = resultadoOperacion
+    }
+
+})
