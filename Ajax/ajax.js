d2b0305392025cc5983e11ed00cb2bfb4a645d2f
CEI <admin@41.local>
2022-05-12 16:21:44 +0200

    Add event load

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -4,10 +4,14 @@ datos.send()
 
 console.log(datos);
 
+datos.addEventListener('load', function(){
+    console.log(datos.responseText);
+})
+
 document.getElementById('boton').addEventListener('click', function(){
     // var persona = datos.responseXML.getElementsByTagName('NOMBRE')
 
-    console.log(datos.responseText.split(' '));
+  
 
     // console.log(persona[0].innerHTML);
 
