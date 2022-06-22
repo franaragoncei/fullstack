dcc2a4f39d93724c93f0091e9a90075cabe6d8da
CEI <admin@41.local>
2022-06-22 18:48:16 +0200

    video scroll

--- jQuery/divImagen/crearImagen.js
+++ jQuery/divImagen/crearImagen.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $('#crearImagen').click(function () {
         html2canvas($('#contenido'), {
             onrendered: function (canvas) {
-                $('#img-out').html('<p>Descargando...</p>')
+                // theCanvas = canvas
+                $('#img-out').html('<p>Descargando....</p>')
 
                 canvas.toBlob(function(blob){
                     saveAs(blob, 'test.png')
